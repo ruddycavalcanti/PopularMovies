e9781fc8394162c8fdbb0779a192762326711341
ruddycavalcanti <ruddycavalcanti@users.noreply.github.com>
2016-03-18 22:21:23 -0300

    First commit

---
+++ app/src/main/java/br/com/popularmovies1/ruddy/popularmovies1/until/Constants.java
@@ -0,0 +1,25 @@
+package br.com.popularmovies1.ruddy.popularmovies1.until;
+
+/**
+ * Constants class store most important strings and paths of the app
+ */
+public final class Constants {
+
+
+    public static final String SORT_BY_POPULAR = "popular";
+    public static final String SORT_BY_MOST_RATED = "top_rated";
+    public static final String API_KEY = "";
+    public static final String URL_API = " https://api.themoviedb.org/3/movie/";
+
+
+    public static final String TITLE_EXTRA = " title";
+    public static final String THEID_EXTRA = " theid";
+
+    public static final String THUMBNAIL_EXTRA = " thumbnail";
+    public static final String DATE_EXTRA = " date_release";
+    public static final String VOTE_EXTRA = " vote_average";
+    public static final String OVERVIEW_EXTRA = " overview";
+
+    public static final String ORIGIN_IMAGE = "http://image.tmdb.org/t/p/";
+    public static final String ORIGIN_IMAGE_SIZE = "w154";
+}
